1c9cf6b33a5772db1ddb181614f5183a2676f45a
hendrik <g9a2hvk9@me.com>
2017-04-12 16:00:48 +0100

    Adds guard condition and raising error to dockingstation

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,10 +2,15 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    Bike.new
+    if @bike == nil #Guard condition if no bikes present
+      raise RuntimeError, "No Bikes!"
+    else
+      Bike.new
+    end
   end
 
-  def park_bike
+  def dock(bike)
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,10 +5,19 @@ describe DockingStation do
 
   it { should respond_to(:release_bike) }
 
-  it { should respond_to(:park_bike) }
+
+  it { should respond_to(:dock).with(1).argument }
 
   it "should release working bike" do
-    expect(subject.release_bike).to be_working
+    if @bike != nil
+      expect(subject.release_bike).to be_working
+    end
+  end
+
+  it "should raise an error" do
+    if @bike == nil
+      expect {subject.release_bike}.to raise_error("No Bikes!")
+    end
   end
 
 
