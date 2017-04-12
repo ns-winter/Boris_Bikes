c627077dc999ba365ba3763bbe67e085187f552e
Nigel Said <nigelsaid@gmail.com>
2017-04-12 14:45:08 +0100

    Adds step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,11 @@
 class DockingStation
+attr_reader :bike
 
   def release_bike
+    Bike.new
   end
+
+  def park_bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,16 @@
 require "docking_station"
 
+
 describe DockingStation do
-  it {respond_to(:release_bike)}
+
+  it { should respond_to(:release_bike) }
+
+  it { should respond_to(:park_bike) }
+
+  it "should release working bike" do
+    expect(subject.release_bike).to be_working
+  end
+
+
+
 end
